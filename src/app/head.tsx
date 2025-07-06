2a445454a492b6de21f8adcd135774296c18c840
patrickd12345 <51156466+patrickd12345@users.noreply.github.com>
2025-07-05 21:04:07 -0400

    Restore global head component for AdSense

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,8 +1,7 @@
 export default function Head() {
   return (
-    <meta
-      name="google-adsense-account"
-      content="ca-pub-2311249346490347"
-    />
+    <>
+      <meta name="google-adsense-account" content="ca-pub-2311249346490347" />
+    </>
   );
 }
