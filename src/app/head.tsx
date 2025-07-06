a21e1fa73e210c0680974d4dbc0c0bdcd01cf28b
patrickd12345 <51156466+patrickd12345@users.noreply.github.com>
2025-07-05 23:04:52 -0400

    chore: add robots.txt and Head component for Google AdSense verification; update ads.txt formatting

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      {/* Google AdSense site verification meta tag */}
+      <meta name="google-adsense-account" content="ca-pub-2311249346490347" />
+    </>
+  )
+} 
